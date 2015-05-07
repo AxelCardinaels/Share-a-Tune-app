c3e377b995578f5443eb2b830b28a080c8efc80c
AxelCardinaels <axel.cardinaels@gmail.com>
2015-05-07 19:17:14 +0200

    Gestion de choix de la source d'importation d'image + Crop des photos

--- Share a Tune/SignUpFirstViewController.swift
+++ Share a Tune/SignUpFirstViewController.swift
@@ -8,17 +8,60 @@
 
 import UIKit
 
-class SignUpFirstViewController: UIViewController {
+class SignUpFirstViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var isCamera = UIImagePickerController.isCameraDeviceAvailable(UIImagePickerControllerCameraDevice(rawValue: 0)!)
+    
     
     @IBOutlet var boutonPhoto: UIButton!
     
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        println("image")
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func choixMode(sender: AnyObject) {
+        
+        
+        var alert = UIAlertController(title: nil, message: "Choisissez la source de votre photo", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        
+        if isCamera == true{
+            alert.addAction(UIAlertAction(title: "Prendre une photo", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                var image = UIImagePickerController()
+                image.delegate = self
+                image.sourceType = UIImagePickerControllerSourceType.Camera
+                image.allowsEditing = true;
+                
+                self.presentViewController(image, animated: true, completion: nil)
+            }))
+        }
+     
+        
+        
+        alert.addAction(UIAlertAction(title: "Choisir une photo enregistrée", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            var image = UIImagePickerController()
+            image.delegate = self
+            image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            image.allowsEditing = true;
+            
+            self.presentViewController(image, animated: true, completion: nil)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Annuler", style: UIAlertActionStyle.Cancel, handler: { (action) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBarHidden = false;
         
-        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.Plain, target: nil, action: nil)
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "Prendre une photo", style: UIBarButtonItemStyle.Plain, target: nil, action: nil)
         
         boutonPhoto.layer.cornerRadius = 0.5 * boutonPhoto.bounds.size.width
         
